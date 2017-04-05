d85f4a52883c62bc1f6d3af067f59474748e3dd4
subarashi <heliojmsemedo@gmail.com>
2017-04-05 14:34:16 +0100

    Initial commit

---
+++ troll.py
@@ -0,0 +1,83 @@
+# collection = ["yo", "collection", "lool", "Ok enough"]
+# print(collection)
+# print(collection.__len__() , len(collection), "lool" in collection) #this determines if "lool" is in collection position 2
+# for i in collection:
+#     print(i)
+
+# for h in range(len(collection)): #this is the true C#' for, the one above is more like foreach i believe
+#     print(collection[h])
+
+# newCollection = list(range(5)) #so this is the constructor??? (uhm so the range(5) returns a collection)
+# newCollection.insert(4,2) #pushes value into the determined index and rearranges order
+# for j in newCollection:
+#     print(j)
+
+
+# print(len(newCollection), " over") #dont forget len(collection) to get lenght
+import readchar
+from pygame import *
+
+class Troll:
+    """Handles trolls and stuffs"""
+    TROLL_ROGUE = "troll:rogue"
+    TROLL_WARRIOR = "troll:warrior"
+    TROLL_SORCERER = "troll:sorc"
+
+    def __init__(self, name: str):
+        self.name = str(name)
+        self.level = 1
+        self.dexterly = 1
+        self.strenght = 1
+        self.magic = 1
+        self.life = 100
+        self.troll_class = "none"
+        self.exp = 0.0
+
+    def init_class(self, troll_class: str):
+        """Initiates Troll class, modifying atributes"""
+        if self.troll_class == "none":
+            self.troll_class = troll_class
+            if troll_class == Troll.TROLL_ROGUE:
+                self.dexterly *= 1.6
+            elif troll_class == Troll.TROLL_SORCERER:
+                self.magic *= 1.6
+            elif troll_class == Troll.TROLL_WARRIOR:
+                self.strenght *= 1.6
+            else:
+                raise ValueError("Specified class does not exist!")
+        else:
+            print("troll is already classed")
+
+    @classmethod
+    def create_classed_troll(cls, troll_class: str, troll_name: str):
+        """Creates a new classed troll"""
+        new_troll = Troll(troll_name)
+        new_troll.init_class(troll_class)
+        return new_troll
+    
+    @classmethod
+    def start_classing(cls, troll):
+        """Starts the classing process"""
+        print("Troll types: \n1 : Warrior \n2 : Rogue \n3 : Sorcerer")
+        usr_class = int(input("Select your class(1-3) and press enter: "))
+        if usr_class == 1:
+            troll.init_class(Troll.TROLL_WARRIOR)
+        elif usr_class == 2:
+            troll.init_class(Troll.TROLL_ROGUE)
+        elif usr_class == 3:
+            troll.init_class(Troll.TROLL_SORCERER)
+        else:
+            raise ValueError("Specified class does not exist!")
+
+USR_NAME = str(input("Name your troll: "))
+USR_TROLL = Troll(USR_NAME)
+print("Do you wish to init your class right away(y/n)? \n\tInit'ing will raise your class's attribute by 60% and speedup exp gain from your class's actions")
+if readchar.readchar() == 'y':
+    Troll.start_classing(USR_TROLL)
+print("test over")
+
+# while(true):
+    
+# mytroll = Troll("Don", 22, 55.1, 90, 59, 4000)
+# yourtroll = Troll("Bungo", 39, 21.2, 194, 29, 6040)
+# Troll.Clash(mytroll, yourtroll)
